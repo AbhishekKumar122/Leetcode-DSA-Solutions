6e5218d309e6287bac31e9632d14390c3c4fd01b
AbhishekKumar122 <abhihariyale22@gmail.com>
2025-04-30 14:30:55 +0530

    Added Two Sum solution in Arrays

---
+++ Arrays/MaxProfit.java
@@ -0,0 +1,21 @@
+package Arrays;
+
+public class MaxProfit {
+    public static int maxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int maxPro = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            maxPro = Math.max(maxPro, prices[i] - minPrice);
+            minPrice = Math.min(prices[i], minPrice);
+        }
+        return maxPro;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+        int result = maxProfit(prices);
+
+        System.out.println(result);
+    }
+}
