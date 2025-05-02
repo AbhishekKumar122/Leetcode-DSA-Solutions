54c75ce58ba4718438e208c313f6d05b9785e7cc
AbhishekKumar122 <abhihariyale22@gmail.com>
2025-05-02 13:21:03 +0530

    Middle Node solution in LinkedList

---
+++ LinkedList/MiddleNode.java
@@ -0,0 +1,53 @@
+package LinkedList;
+
+import java.util.LinkedList;
+
+public class MiddleNode {
+
+    // Definition for singly-linked list.
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static ListNode middleNode(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        // Call middleNode
+        ListNode middle = middleNode(head);
+
+        System.out.println("Middle node is: " + middle.val);
+    }
+}
