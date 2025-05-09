515762f960fb87909a5bfea12eeb2a604396563f
AbhishekKumar122 <abhihariyale22@gmail.com>
2025-05-09 16:54:32 +0530

    Palindrome LinkedList

---
+++ LinkedList/PalindromeList.java
@@ -0,0 +1,52 @@
+package LinkedList;
+
+import java.util.ArrayList;
+
+public class PalindromeList {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public static boolean isPalindrome(ListNode head) {
+
+        ArrayList<Integer> list = new ArrayList<>();
+        while (head != null) {
+            list.add(head.val);
+            head = head.next;
+        }
+
+        int left = 0;
+        int right = list.size() - 1;
+
+        while (left < right && list.get(left) == list.get(right)) {
+            left++;
+            right--;
+        }
+        return left >= right;
+
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(2);
+        head.next.next.next = new ListNode(1);
+
+        boolean result = isPalindrome(head);
+        System.out.println(result);
+    }
+}
